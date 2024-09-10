fd81bc29abc0278ee1986f9d1972a16c6b24677f
xjano <jano.jb2108@gmail.com>
2024-09-10 17:45:50 +0200

    Implemented HIGHS as MVP

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    webpack: {
+      configure: {
+        resolve: {
+          fallback: {
+            fs: false, // `fs` wird in der Browserumgebung nicht benötigt
+            path: require.resolve("path-browserify"),
+          },
+        },
+      },
+    },
+  };
+  
