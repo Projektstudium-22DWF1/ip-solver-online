6ee3247200b485fee36bcae3e202265fa1abc3f1
xjano <jano.jb2108@gmail.com>
2024-09-11 19:45:43 +0200

    GLPK working maybe?

--- src/components/GlpkSolver.js
+++ src/components/GlpkSolver.js
@@ -9,31 +9,20 @@ const GlpkSolver = () => {
     const solveProblem = () => {
         try {
             var str = `param n > 0 integer;
-
                         param log2_n := log(n) / log(2);
-
                         param k := floor(log2_n);
-
                         param a{j in 1..n} := 2 ** (k + n + 1) + 2 ** (k + n + 1 - j) + 1;
-
                         param b := 0.5 * floor(sum{j in 1..n} a[j]);
-
                         var x{1..n} binary;
-
                         maximize obj: sum{j in 1..n} a[j] * x[j];
-
                         s.t. cap: sum{j in 1..n} a[j] * x[j] <= b;
-
                         data;
-
                         param n := 15;
-
                         end;`
 
             var tran = glpk.glp_mpl_alloc_wksp();
-
             var pos = 0;
-            var test = glpk.glp_mpl_read_model(tran, null,
+            glpk.glp_mpl_read_model(tran, null,
                 function () {
                     if (pos < str.length) {
                         return str[pos++];
@@ -42,8 +31,31 @@ const GlpkSolver = () => {
                 },
             )
 
-            const result = "";
-            setOutputData(result, test);
+
+            var lp = glpk.glp_create_prob();
+            glpk.glp_mpl_generate(tran, null, console.log);
+            glpk.glp_mpl_build_prob(tran, lp);
+            glpk.glp_scale_prob(lp);
+            var smcp = new glpk.SMCP({presolve: glpk.GLP_ON});
+
+            glpk.glp_simplex(lp, smcp);
+        
+            var iocp = new glpk.IOCP({
+                presolve: glpk.GLP_ON,
+                cb_func: function(tree){
+                    if (glpk.glp_ios_reason(tree) == glpk.GLP_IBINGO){
+                        var objective = glpk.glp_mip_obj_val(glpk.glp_ios_get_prob(tree));
+                        //console.log("@@@" + objective);
+        
+                    }
+                }
+            });
+        
+            glpk.glp_intopt(lp, iocp);
+            glpk.glp_mpl_postsolve(tran, lp, glpk.GLP_MIP);
+            console.log("obj: " + glpk.glp_mip_obj_val(lp));
+            
+            setOutputData("");
         } catch (error) {
             setOutputData(`Fehler: ${error.message}`);
         }
