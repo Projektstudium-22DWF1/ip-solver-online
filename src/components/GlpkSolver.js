f13522e21f82f522be86c5ff88b8b5b87b5e5699
Felix Grothaus <felix.grothaus94@gmail.com>
2024-09-11 22:22:43 +0200

    Quelltext leicht abgeändert. Läuft jetzt genauso, wie der HighsSolver

--- src/components/GlpkSolver.js
+++ src/components/GlpkSolver.js
@@ -6,9 +6,33 @@ const GlpkSolver = () => {
     const [inputData, setInputData] = useState("");
     const [outputData, setOutputData] = useState(null);
 
-    const solveProblem = () => {
-        try {
-            var str = `param n > 0 integer;
+    var complexProb = `set I;
+                        set J;
+                        param a{i in I};
+                        param b{j in J};
+                        param d{i in I, j in J};
+                        param f;
+                        param c{i in I, j in J} := f * d[i,j] / 1000;
+                        var x{i in I, j in J} >= 0;
+                        minimize cost: sum{i in I, j in J} c[i,j] * x[i,j];
+                        s.t. supply{i in I}: sum{j in J} x[i,j] <= a[i];
+                        s.t. demand{j in J}: sum{i in I} x[i,j] >= b[j];
+                        data;
+                        set I := Seattle San-Diego;
+                        set J := New-York Chicago Topeka;
+                        param a := Seattle     350
+                                   San-Diego   600;
+                        param b := New-York    325
+                                   Chicago     300
+                                   Topeka      275;
+                        param d :              New-York   Chicago   Topeka :=
+                                   Seattle     2.5        1.7       1.8
+                                   San-Diego   2.5        1.8       1.4  ;
+                        param f := 90;
+                        end;`;
+
+
+    var str = `param n > 0 integer;
                         param log2_n := log(n) / log(2);
                         param k := floor(log2_n);
                         param a{j in 1..n} := 2 ** (k + n + 1) + 2 ** (k + n + 1 - j) + 1;
@@ -20,12 +44,15 @@ const GlpkSolver = () => {
                         param n := 15;
                         end;`
 
+    const solveProblem = (problem) => {
+        try {
+
             var tran = glpk.glp_mpl_alloc_wksp();
             var pos = 0;
             glpk.glp_mpl_read_model(tran, null,
                 function () {
-                    if (pos < str.length) {
-                        return str[pos++];
+                    if (pos < problem.length) {
+                        return problem[pos++];
                     } else
                         return -1;
                 },
@@ -55,7 +82,7 @@ const GlpkSolver = () => {
             glpk.glp_mpl_postsolve(tran, lp, glpk.GLP_MIP);
             console.log("obj: " + glpk.glp_mip_obj_val(lp));
             
-            setOutputData("");
+            setOutputData(glpk.glp_mip_obj_val(lp));
         } catch (error) {
             setOutputData(`Fehler: ${error.message}`);
         }
@@ -69,8 +96,12 @@ const GlpkSolver = () => {
                 value={inputData}
                 onChange={(e) => setInputData(e.target.value)}
                 placeholder="Gib dein Optimierungsproblem hier ein..."
-            />
-            <button className="solve-button" onClick={solveProblem}>Problem lösen</button>
+            >
+                {complexProb}
+            </textarea>
+            <button className="solve-button" onClick={() => solveProblem(inputData)}>
+                Problem lösen
+            </button>
 
             {outputData && (
                 <div className="output-container">
@@ -78,7 +109,36 @@ const GlpkSolver = () => {
                     <pre className="output-data">{outputData}</pre>
                 </div>
             )}
+            <br/>
+            <pre>
+                {` Generisches Problem aus ILIAS:
+                set I;
+                set J;
+                param a{i in I};
+                param b{j in J};
+                param d{i in I, j in J};
+                param f;
+                param c{i in I, j in J} := f * d[i,j] / 1000;
+                var x{i in I, j in J} >= 0;
+                minimize cost: sum{i in I, j in J} c[i,j] * x[i,j];
+                s.t. supply{i in I}: sum{j in J} x[i,j] <= a[i];
+                s.t. demand{j in J}: sum{i in I} x[i,j] >= b[j];
+                data;
+                set I := Seattle San-Diego;
+                set J := New-York Chicago Topeka;
+                param a := Seattle     350
+                           San-Diego   600;
+                param b := New-York    325
+                           Chicago     300
+                           Topeka      275;
+                param d :              New-York   Chicago   Topeka :=
+                           Seattle     2.5        1.7       1.8
+                           San-Diego   2.5        1.8       1.4;
+                param f := 90;
+                end;`}
+            </pre>
         </div>
+        
     );
 };
 
