972f552d18014fc4c87eff23656466d5a21f72fc
Ben Hagmann <benhag02@gmail.com>
2024-10-04 13:14:36 +0200

    fixed navigation bar

--- src/components/Tabs/MainNavbar.jsx
+++ src/components/Tabs/MainNavbar.jsx
@@ -3,7 +3,7 @@ import { LanguageContext } from "../../context/LanguageContext";
 import "uikit/dist/css/uikit.min.css";
 import { SolverTab } from "./SolverTab";
 import DescriptionTab from "./DescriptionTab";
-import LanguageSelector from "./LanguageSelector"; // Importiere die neue Komponente
+import LanguageSelector from "./LanguageSelector"; // Import the LanguageSelector component
 
 export function MainNavbar() {
   const [activeComponent, setActiveComponent] = useState("SolverTab");
@@ -12,8 +12,11 @@ export function MainNavbar() {
   return (
     <React.Fragment>
       <div>
-        {/* Navigationsleiste */}
-        <nav className="uk-navbar-container" uk-navbar={"true"}>
+        {/* Sticky Navbar */}
+        <nav 
+          className="uk-navbar-container" 
+          uk-navbar="true" 
+          uk-sticky="animation: uk-animation-slide-top; sel-target: .uk-navbar; cls-active: uk-navbar-sticky; top: 0; z-index: 1000;">
           <div className="uk-navbar-left">
             <ul className="uk-navbar-nav">
               <li className={`uk-active ${activeComponent === "SolverTab" ? "active" : ""}`}>
@@ -31,12 +34,12 @@ export function MainNavbar() {
 
           {/* Language Selector */}
           <div className="uk-navbar-right">
-            <LanguageSelector /> {/* Verwende die ausgelagerte Komponente */}
+            <LanguageSelector />
           </div>
         </nav>
 
-        {/* Laden der Komponente */}
-        <div className="uk-container" id={"all"}>
+        {/* Main content */}
+        <div className="uk-container" id="all" style={{ paddingTop: "80px" }}>
           {activeComponent === "SolverTab" && <SolverTab />}
           {activeComponent === "DescriptionTab" && <DescriptionTab />}
         </div>
