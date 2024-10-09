9bb5990b054e479d2681a71f4bb6335c0b3b566f
Ben Hagmann <benhag02@gmail.com>
2024-10-09 12:14:04 +0200

    fixed unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
+// App.test.js
+
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+// Mock the glpk.min.js module to avoid GLPK-related errors during tests
+jest.mock("./dist/glpk.min.js", () => ({
+  solve: jest.fn(() => Promise.resolve({}))
+}));
+
+test("renders the homepage title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const homepageTitle = screen.getByText(/Optimized Solutions/i); 
+  expect(homepageTitle).toBeInTheDocument();
 });
