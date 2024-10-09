57f90f7f00940c85ad5e01651ec44177f31097cd
Gust-av <jonasgottschalk111@yahoo.de>
2024-10-09 08:47:01 +0200

    Add CSS styling for landingpage button

--- src/components/Tabs/LandingPageTab.jsx
+++ src/components/Tabs/LandingPageTab.jsx
@@ -11,7 +11,9 @@ function LandingPageTab({ setActiveComponent }) {
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "calc(100vh - 50px)" }}>
         <h1>{translations.landingpageheader}</h1>
         <p>{translations.landingpagetext}</p>
-        <button onClick={() => setActiveComponent("SolverTab")}>{translations.landingpagebutton}</button>
+        <button className="landing-page-button" onClick={() => setActiveComponent("SolverTab")}>
+          {translations.landingpagebutton}
+        </button>
       </div>
     </React.Fragment>
   );
